9de69f457562c9543dbbbf5256c6c562117c3dbc
Rimuru <whitewalls@naver.com>
2021-03-23 00:10:45 +0900

    로그인 회원가입 구현

--- server/index.js
+++ server/index.js
@@ -27,6 +27,8 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요~~~'))
 
+app.get('/api/hello', (req, res) => res.send('Hello World!~~'))
+
 app.post('api/users/register', (req, res) => {
     // 회원 가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -41,7 +43,7 @@ app.post('api/users/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) {
